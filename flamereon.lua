606f644d0d2fb5c1de630686dbb9e3c4fb589874
Loqrp <loqrpreal@gmail.com>
2024-09-13 17:39:01 +0300

    Update flamereon.lua

--- flamereon.lua
+++ flamereon.lua
@@ -5,7 +5,7 @@ local LocalPlayer = game:GetService("Players").LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 local HttpService = game:GetService("HttpService")
 
--- balls xxx
+-- BallsX
 
 local OrionLib = {
 	Elements = {},
