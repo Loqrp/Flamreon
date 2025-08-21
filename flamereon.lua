ec905de131517134048d780896ec754d80c27ec8
Loqrp <loqrpreal@gmail.com>
2025-08-21 19:30:37 +0300

    bbbbb

--- flamereon.lua
+++ flamereon.lua
@@ -712,7 +712,7 @@ function OrionLib:MakeWindow(WindowConfig)
 		TweenService:Create(LoadSequenceText, TweenInfo.new(.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextTransparency = 0}):Play()
 		wait(2)
 		TweenService:Create(LoadSequenceText, TweenInfo.new(.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
-		MainWindow.Visible = true
+		MainWindow.Visible = false
 		LoadSequenceLogo:Destroy()
 		LoadSequenceText:Destroy()
 	end 
